74c611c4897c5cb09eaf60fabb1a53b12ed7e3f7
Sam Thomson <samt@samt.st>
2019-04-15 09:37:17 +0100

    server serves..

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,2 +1,22 @@
-// @ts-ignore
-console.log('this is in the server')
+import * as express from 'express'
+
+const app = express()
+const port = process.env.PORT
+
+// app.use(bodyParser.json());
+// app.use('/graphql', expressGraphQL({
+//   schema,
+//   graphiql: true
+// }));
+
+// const webpackMiddleware = require('webpack-dev-middleware');
+// const webpack = require('webpack');
+// const webpackConfig = require('../webpack.config.js');
+// app.use(webpackMiddleware(webpack(webpackConfig)));
+
+app.get('/', (req, res) => res.send('expense explorer - home root'))
+
+app.listen(
+	port,
+	() => console.log(`Example app listening on port ${port}!`)
+)
