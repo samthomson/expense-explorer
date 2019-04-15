509d84f397177cd0c70da61a8dadf785c8a9ae25
Sam Thomson <samt@samt.st>
2019-04-15 15:29:09 +0100

    read in csv

--- server/importer/import.ts
+++ server/importer/import.ts
@@ -1,5 +1,19 @@
 import * as path from 'path'
 import * as fs from 'fs'
+import * as csv from 'csv-parser'
+
+export type Expense = {
+	Date: string //'06/01/2018',
+	Type: string //'N/A',
+	Category: string //'Food/Drink',
+	Subcategory: string // 'Fast Food',
+	Vendor: string // 'Pad thai',
+	Payment: string // 'N/A',
+	Currency: string // 'DKK',
+	Amount: number // '-76,55',
+	Note: string // 'GBP: 9,00 £\nExchange rate: 8.50545829\n',
+	ID: string // '6B1A25B8-5BCD-431A-9190-73F32F91ACC3' }
+}
 
 const main  = async () => {
 	// @ts-ignore
@@ -15,6 +29,36 @@ const main  = async () => {
 	}
 	const sImportFile: string = path.resolve(sImportDir, asFiles[0])
 	console.log('import: ', sImportFile)
+
+
+	const results: Expense[] = await readInFile(sImportFile)
+	
+	console.log(results.length, ' items')
+	console.log(results[0])
+
+	let iSum: number = results.reduce((iTotal: number, oExpense: Expense) => iTotal + oExpense.Amount, 0)
+
+	console.log('iSum: ', iSum)
 }
 
 main()
+
+async function readInFile (sImportFile: string) {
+	return new Promise<Expense[]>(resolve => {
+
+		let results: Expense[] = []
+
+		fs.createReadStream(sImportFile)
+			.pipe(csv())
+			.on('data', (data: any) => {
+				let fAmount: number = parseFloat(data.Amount)
+				// console.log(fAmount)
+				return results.push({
+				...data,
+				Amount: fAmount
+			})})
+			.on('end', () => {
+				resolve(results)
+			})
+	})
+}
