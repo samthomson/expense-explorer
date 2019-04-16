3d7685cafb531b6161bdfcced9dafff3717ec808
Sam Thomson <samt@samt.st>
2019-04-16 13:42:00 +0100

    tabulated expense list

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -53,10 +53,26 @@ class App extends React.Component<IAppProps, {}> {
 						<p>total expenses: {numberOfExpenses}</p>
 
 						<hr />
-						{expenses.map(
-							(oSingleExpense, i) => <div key={i}>
-							{oSingleExpense.amount} {oSingleExpense.vendor}{oSingleExpense.category}{oSingleExpense.subcategory}
-							</div>
+						{expenses.length > 0 && (
+							<table>
+								<thead>
+									<tr><th>dkk (usd)</th><th>item</th><th>category</th><th>subcategory</th></tr>
+								</thead>
+								<tbody>
+									{expenses.map(
+										(oSingleExpense, i) => {
+											return (
+												<tr key={i}>
+													<td>{oSingleExpense.amount} (?)</td>
+													<td>{oSingleExpense.vendor}</td>
+													<td>{oSingleExpense.category}</td>
+													<td>{oSingleExpense.subcategory}</td>
+												</tr>
+											)
+										}
+									)}
+								</tbody>
+							</table>
 						)}
 					</div>
 				)}
