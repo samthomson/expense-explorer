fb26fd9152df569ed9c600acd64cf34aac14d501
Sam Thomson <samt@samt.st>
2019-04-26 08:50:57 +0100

    render all subcategories

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -296,28 +296,12 @@ class App extends React.Component<IAppProps, {}> {
 			maintainAspectRatio: false
 		}
 
-		// https://flatuicolors.com/palette/cn
-		// https://flatuicolors.com/palette/nl
-		const oCategoryColours: any = {
-			'accomodation': '#EA2027', // red - red pigment	
-			'food': '#2ed573', // green - ufo green
-			'working': '#a4b0be', // grey - peace
-			'recreation': '#1e90ff', // blue - clear chill
-			'transport': '#5758BB', // lavendar - circumorbital ring
-			'non-food shopping': '#F79F1F', // orangish/gold - radiant yellow
-			'miscellaneous': '#833471', // light purple - hollyhock
-			'health': '#ff6b81', // pink - wild watermelon
-			'utility': '#747d8c', // darker grey - bay wharf
-			'giving': '#7bed9f' // light green - lime soap
-		}
-
 		const chartData = categories.map(oP => {
 			console.log(`category: ${oP.category}`)
 			return {
 				value: (Number(oP.percent)).toFixed(0),
-				// label: `"${oP.category}"`,
 				label: oP.category,
-				color: oCategoryColours[oP.category] ? oCategoryColours[oP.category] : 'white'
+				color: `hsla(${Math.random() * 360}, 100%, 50%, 1)`
 			}
 		})
 
