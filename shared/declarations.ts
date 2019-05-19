850ce25b9768c0f3543e86cacd4c08718a2b3829
Sam Thomson <samt@samt.st>
2019-05-19 06:00:05 +0000

    structure for sharing stuff

---
+++ shared/declarations.ts
@@ -0,0 +1,21 @@
+export type Summary = {
+	spending_by_category: any[]
+	spending_by_subcategory: any[]
+	spending_over_time: any[]
+	projected_spending_over_time: any[]
+	average_per_unit: number
+	median_per_unit: number
+	mode_per_unit: number
+	projection_for_scope: number
+	expenses: Expense[]
+	totalExpenditure: number
+	numberOfExpenses: number
+	prospective_budget_for_forecast: number
+}
+export type Expense = {
+	amount: number
+	vendor: string
+	category: string
+	subcategory: string
+	date: string
+}
