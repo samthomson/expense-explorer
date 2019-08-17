e33d9827468a97f272409745138c34a94680a386
Sam Thomson <samt@samt.st>
2019-08-17 07:46:12 +0300

    bash script to start everything

---
+++ sai.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# start the app, and run the importer, then open a web browser
+
+docker-compose up -d
+
+docker-compose run server yarn run import
+
+# linux - others here: https://stackoverflow.com/questions/38147620/shell-script-to-open-a-url#38147878
+xdg-open http://localhost:3400
+
