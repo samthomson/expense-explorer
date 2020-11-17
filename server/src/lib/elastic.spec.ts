545ad8f0a0b4e4e7661fea78f42e720bc3761462
Sam Thomson <samt@samt.st>
2020-11-17 14:27:49 +0000

    stub test file

---
+++ server/src/lib/elastic.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from 'chai'
+import 'mocha'
+
+import * as ElasticUtil from './elastic'
+
+describe('elastic util', async () => {
+	it('determines median per period correctly', async () => {
+		expect(true).to.equal(false)
+	})
+})
