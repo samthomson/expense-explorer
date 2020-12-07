bb004de45db1daa59d3cd73e5f180ddb00ea6158
Sam Thomson <samt@samt.st>
2020-12-07 16:15:05 +0000

    refacotr App.tsx to pure component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,11 +15,11 @@ import SpendingSummary from 'src/components/SpendingSummary'
 
 import {
 	Action,
-	changeMonth,
-	changeScope,
-	getSummary,
-	setBudget,
-	setFilter,
+	changeMonth as changeMonthAction,
+	changeScope as changeScopeAction,
+	getSummary as getSummaryAction,
+	setBudget as setBudgetAction,
+	setFilter as setFilterAction,
 } from 'src/redux/actions'
 import { Store } from 'src/redux/store'
 import './../node_modules/semantic-ui-css/semantic.min.css'
@@ -37,148 +37,141 @@ interface IAppProps {
 	setFilter: (oSummary: Filter | null) => {}
 }
 
-class App extends React.Component<IAppProps, {}> {
-	public renderError() {
-		return '...no data.. (loading?)'
-	}
-
-	public renderDateInputUI() {
-		return (
-			<div className="ui small buttons right floated">
-				<button
-					className="ui labeled icon button"
-					onClick={() => this.eChangeMonth(true)}
-				>
-					<i className="left chevron icon" />
-					Back
-				</button>
-				<button
-					className="ui right labeled icon button"
-					onClick={() => this.eChangeMonth(false)}
-				>
-					Forward
-					<i className="right chevron icon" />
-				</button>
-			</div>
-		)
-	}
-
-	public renderEverything() {
-		const { initialDate, filter, oSummary, sScope } = this.props
-		const {
-			spendingByCategory,
-			spendingBySubcategory,
-			spendingOverTime,
-			expenses,
-			totalExpenditure,
-		}: Summary = oSummary
-		return (
-			<div className="App ui container">
-				<h1>expense explorer</h1>
-				<div className="ui grid">
-					<div className="three column row">
-						<div className="column">
-							{/* month / year changer */}
-							<ScopeInput scope={sScope} changeScope={(newScope: SharedTypes.Scope) => this.eChangeScope(newScope)} />
-							{sScope === 'custom' && <DateEntry />}
-						</div>
-						<div className="column centered-text">
-							{/* current period */}
-							<h2>
-								{this.renderScopeLabel(initialDate, sScope)}
-								{filter && (
-									<span>
-										&nbsp;(&nbsp;
-										{filter.term}:{filter.match}
-										<a onClick={() => this.eRemoveFilter()}>
-											<i className="icon trash" />
-										</a>
-										&nbsp;)
-									</span>
-								)}
-							</h2>
-						</div>
-						<div className="column">
-							{/* date navigation */}
-							{this.renderDateInputUI()}
-						</div>
-					</div>
-				</div>
-
-				{/* render expenses for current date */}
-				{!!totalExpenditure && totalExpenditure > 0 && spendingOverTime && spendingByCategory && spendingBySubcategory && expenses && oSummary.averagePerUnit && (
-					<div>
-						<br />
-						<SpendingSummary oSummary={this.props.oSummary} nYearlyBudget={this.props.nYearlyBudget} scope={this.props.sScope} initialDate={this.props.initialDate} setBudget={this.eChangeBudget} />
-						<SpendingOverTime initialDate={initialDate} scope={sScope} summary={oSummary} timeunits={spendingOverTime} />
-						<br />
-						<CategoryExpenses
-							categories={spendingByCategory}
-							eSetFilter={this.eSetFilter}
-							sCategoryName={'Category'}
-						/>
-						<br />
-						<CategoryExpenses
-							categories={spendingBySubcategory}
-							eSetFilter={this.eSetFilter}
-							sCategoryName={'Subcategory'}
-						/>
-						<br />
-						<div>
-							<h3>Expenses</h3>
-							<ExpenseTable
-								eSetFilter={this.eSetFilter}
-								expenses={expenses}
-							/>
-						</div>
-					</div>
-				)}
-				{totalExpenditure === 0 && (
-					<div>
-						<br />
-						<h2 className="centered-text">
-							no expense data for current period..
-						</h2>
-					</div>
-				)}
-			</div>
-		)
-	}
-
-	public render() {
-		return this.props.oSummary
-			? this.renderEverything()
-			: this.renderError()
-	}
-
-	private eChangeMonth(bBackwards: boolean) {
-		this.props.changeMonth(bBackwards)
-		this.props.getSummary()
-	}
-
-	private eChangeScope(sScope: string) {
-		this.props.changeScope(sScope)
-		this.props.getSummary()
-	}
-
-	private eChangeBudget(fBudget: number) {
-		this.props.setBudget(fBudget)
-		this.props.getSummary()
-	}
-
-	private renderScopeLabel(date: moment.Moment, sScope: string) {
-		const sFormat: string = sScope === 'month' ? 'MMMM YYYY' : 'Y'
+const App: React.StatelessComponent<IAppProps> = ({initialDate, filter, oSummary, sScope, nYearlyBudget, changeMonth, changeScope, getSummary, setBudget, setFilter}) => {
+	
+	
+	const scopeLabel = (date: moment.Moment, currentScope: string) => {
+		const sFormat = currentScope === 'month' ? 'MMMM YYYY' : 'Y'
 		return date.format(sFormat)
 	}
 
-	private eSetFilter = (term: string, match: string) => {
-		this.props.setFilter({ term, match })
-		this.props.getSummary()
+
+	const eChangeMonth = (bBackwards: boolean) => {
+		changeMonth(bBackwards)
+		getSummary()
 	}
-	private eRemoveFilter = () => {
-		this.props.setFilter(null)
-		this.props.getSummary()
+
+	const eChangeScope = (newScope: SharedTypes.Scope) => {
+		changeScope(newScope)
+		getSummary()
 	}
+
+	const eChangeBudget = (fBudget: number) => {
+		setBudget(fBudget)
+		getSummary()
+	}
+
+	const eSetFilter = (term: string, match: string) => {
+		setFilter({ term, match })
+		getSummary()
+	}
+	
+	const eRemoveFilter = () => {
+		setFilter(null)
+		getSummary()
+	}
+
+	
+
+	if (!oSummary) {
+		return (<React.Fragment>...no data.. (loading?)</React.Fragment>)
+	}
+
+	const {
+		spendingByCategory,
+		spendingBySubcategory,
+		spendingOverTime,
+		expenses,
+		totalExpenditure,
+	}: Summary = oSummary
+
+	return (
+		<div className="App ui container">
+			<h1>expense explorer</h1>
+			<div className="ui grid">
+				<div className="three column row">
+					<div className="column">
+						{/* month / year changer */}
+						<ScopeInput scope={sScope} changeScope={(newScope: SharedTypes.Scope) => eChangeScope(newScope)} />
+						{sScope === 'custom' && <DateEntry />}
+					</div>
+					<div className="column centered-text">
+						{/* current period */}
+						<h2>
+							{scopeLabel(initialDate, sScope)}
+							{filter && (
+								<span>
+									&nbsp;(&nbsp;
+									{filter.term}:{filter.match}
+									<a onClick={() => eRemoveFilter()}>
+										<i className="icon trash" />
+									</a>
+									&nbsp;)
+								</span>
+							)}
+						</h2>
+					</div>
+					<div className="column">
+						{/* date navigation */}
+						<div className="ui small buttons right floated">
+							<button
+								className="ui labeled icon button"
+								onClick={() => eChangeMonth(true)}
+							>
+								<i className="left chevron icon" />
+								Back
+							</button>
+							<button
+								className="ui right labeled icon button"
+								onClick={() => eChangeMonth(false)}
+							>
+								Forward
+								<i className="right chevron icon" />
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			{/* render expenses for current date */}
+			{!!totalExpenditure && totalExpenditure > 0 && spendingOverTime && spendingByCategory && spendingBySubcategory && expenses && oSummary.averagePerUnit && (
+				<div>
+					<br />
+					<SpendingSummary oSummary={oSummary} nYearlyBudget={nYearlyBudget} scope={sScope} initialDate={initialDate} setBudget={eChangeBudget} />
+					<SpendingOverTime initialDate={initialDate} scope={sScope} summary={oSummary} timeunits={spendingOverTime} />
+					<br />
+					<CategoryExpenses
+						categories={spendingByCategory}
+						eSetFilter={eSetFilter}
+						sCategoryName={'Category'}
+					/>
+					<br />
+					<CategoryExpenses
+						categories={spendingBySubcategory}
+						eSetFilter={eSetFilter}
+						sCategoryName={'Subcategory'}
+					/>
+					<br />
+					<div>
+						<h3>Expenses</h3>
+						<ExpenseTable
+							eSetFilter={eSetFilter}
+							expenses={expenses}
+						/>
+					</div>
+				</div>
+			)}
+			{totalExpenditure === 0 && (
+				<div>
+					<br />
+					<h2 className="centered-text">
+						no expense data for current period..
+					</h2>
+				</div>
+			)}
+		</div>
+	)
+
 }
 
 const mapStateToProps = (state: Store.App) => {
@@ -193,11 +186,11 @@ const mapStateToProps = (state: Store.App) => {
 }
 
 const mapDispatchToProps = (dispatch: React.Dispatch<Action>) => ({
-	changeMonth: (bBackwards: boolean) => dispatch(changeMonth(bBackwards)),
-	changeScope: (sScope: string) => dispatch(changeScope(sScope)),
-	getSummary: () => dispatch(getSummary()),
-	setBudget: (fYearlyBudget: number) => dispatch(setBudget(fYearlyBudget)),
-	setFilter: (filter: Filter | null) => dispatch(setFilter(filter)),
+	changeMonth: (bBackwards: boolean) => dispatch(changeMonthAction(bBackwards)),
+	changeScope: (sScope: string) => dispatch(changeScopeAction(sScope)),
+	getSummary: () => dispatch(getSummaryAction()),
+	setBudget: (fYearlyBudget: number) => dispatch(setBudgetAction(fYearlyBudget)),
+	setFilter: (filter: Filter | null) => dispatch(setFilterAction(filter)),
 })
 
 export default connect(
