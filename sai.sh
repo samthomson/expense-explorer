0180b7ef69e934137a8d9841031b2684d3409957
sam <samt@samt.st>
2023-01-23 11:42:24 +0700

    use open command for mac

--- sai.sh
+++ sai.sh
@@ -7,5 +7,5 @@ docker-compose up -d
 docker-compose run server yarn run import
 
 # linux - others here: https://stackoverflow.com/questions/38147620/shell-script-to-open-a-url#38147878
-xdg-open http://localhost:3400
+open http://localhost:3400
 
